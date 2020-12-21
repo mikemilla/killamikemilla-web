efc9d70679c8b4ed036a554e82d5bc20400e1b6b
Michael Miller <mike@mikemiller.design>
2020-12-21 16:53:52 -0500

    Final styles

--- src/components/FeaturedItem.tsx
+++ src/components/FeaturedItem.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FeaturedItem(data: any) {
     return (
-        <a href={data.entry.link}>
+        <a href={data.entry.link} target={data.entry.openNewTab ? '_blank' : ''}>
             <div className={data.entry.isLast ? 'FeaturedItem isLast' : 'FeaturedItem'}>
                 <img src={data.entry.imageUrl} />
                 <p>{data.entry.title}</p>
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
 function ListItem(data: any) {
-
-    console.log(data)
-
     return (
-        <a href={data.entry.link}>
+        <a href={data.entry.link} target={data.entry.openNewTab ? '_blank' : ''}>
             <div className={data.entry.isLast ? 'ListItem isLast' : 'ListItem'}>
                 <img src={data.entry.imageUrl} />
                 <p>{data.entry.title}</p>
